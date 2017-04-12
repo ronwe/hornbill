8d0c642f4dad917fc56073d535b7fce6e2de1965
荣威 <weiwei.rong@corp.elong.com>
2017-04-12 16:45:07 +0800

    no message

--- frame/index.js
+++ frame/index.js
@@ -28,27 +28,6 @@ exports.use = function(connect , opt){
  */
 function startService(options){
 	server.start(options)
-	/*
-	options = options || {}
-	var numCPUs = options.cpuNums || require('os').cpus().length
-
-	cluster.setupMaster({
-		exec : path.resolve(__dirname,'./server.js'),
-		args : [options.configPath  , options.appsPath],
-		silent : false
-	})
-
-	for(var i = numCPUs ; i--;){
-		cluster.fork()
-	}
-
-	cluster.on('exit', function(worker) {
-		var st = new Date
-		st = st.getFullYear()+ '-'+ (st.getMonth()+1)+ '-'+st.getDate()+ ' '+st.toLocaleTimeString()
-		console.log('worker ' + worker.process.pid + ' died at:',st);
-		cluster.fork()
-	 })
-	 */
 }
 process.on('uncaughtException', function (err) {
     console.log(err)
