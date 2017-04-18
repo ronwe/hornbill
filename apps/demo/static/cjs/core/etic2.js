e778781a84717fb7b6a4a482b08f3913bca53a21
荣威威 <weiwei.rong@corp.elong.com>
2017-04-18 11:32:27 +0800

    InsertStatic 增加服务端直接编译功能

--- apps/demo/static/cjs/core/etic2.js
+++ apps/demo/static/cjs/core/etic2.js
@@ -12,38 +12,43 @@ function etic(tplId , data){
     if (cache[tplId]) return cbk()
 
     var tpl = tplNode.innerHTML
-    var con = 'var ' + sn + ' ="" ;'
-    function tLine(str ){
-        return str.replace(/[\r\t\n]/g, " ").replace(/'/g , "\\'")
-    }
 
-    while (true){
-        var sPos = tpl.indexOf('<?')
-        if (-1 == sPos) break
-        var ePos = tpl.indexOf('?>' , sPos + 2)
+	if ('text/template-x' == tplNode.getAttribute('type')) {
+		var con = tpl	
+	}else {
+		var con = 'var ' + sn + ' ="" ;'
+		function tLine(str ){
+			return str.replace(/[\r\t\n]/g, " ").replace(/'/g , "\\'")
+		}
 
-        var part1 = tpl.slice(0,sPos)
-            , f = tpl.slice(sPos + 2 , ePos)
-            ,tpl = tpl.slice(ePos + 2)
-        var op = f.charAt(0)
-        if (part1.length) con += sn + " += '" + tLine(part1) + "';"
-        switch (op){
-            case '=' :
-                f = f.slice(1)
-                con += sn + " += " + f + ";"
-                break
-            case '#' :
-                f = f.slice(1).trim()
-                con += sn +  " += etic2('" + f + "')(this);"
-                break
-            default:
-                con += f
-        }
+		while (true){
+			var sPos = tpl.indexOf('<?')
+			if (-1 == sPos) break
+			var ePos = tpl.indexOf('?>' , sPos + 2)
 
-    }
+			var part1 = tpl.slice(0,sPos)
+				, f = tpl.slice(sPos + 2 , ePos)
+				,tpl = tpl.slice(ePos + 2)
+			var op = f.charAt(0)
+			if (part1.length) con += sn + " += '" + tLine(part1) + "';"
+			switch (op){
+				case '=' :
+					f = f.slice(1)
+					con += sn + " += " + f + ";"
+					break
+				case '#' :
+					f = f.slice(1).trim()
+					con += sn +  " += etic2('" + f + "')(this);"
+					break
+				default:
+					con += f
+			}
 
-    tpl.length  && (con += sn + " += '" + tLine(tpl) + "';")
-    con += 'return ' + sn
+		}
+
+		tpl.length  && (con += sn + " += '" + tLine(tpl) + "';")
+		con += 'return ' + sn
+	}
     try{
         var t = new Function("" , con)
         cache[tplId] = function (data){ return t.call(data)}
--- frame/lib/est/extFn.js
+++ frame/lib/est/extFn.js
@@ -19,10 +19,48 @@ plugins.forEach(function(plug){
 })
 exports.plugin = _plug
 
+function etic(tpl){
+	var sn = '_ret_'  
+    var con = 'var ' + sn + ' ="" ;'
+    function tLine(str ){
+        return str.replace(/[\r\t\n]/g, " ").replace(/'/g , "\\'")
+    }
+
+    while (true){
+        var sPos = tpl.indexOf('<?')
+        if (-1 == sPos) break
+        var ePos = tpl.indexOf('?>' , sPos + 2)
+
+        var part1 = tpl.slice(0,sPos)
+            , f = tpl.slice(sPos + 2 , ePos)
+            ,tpl = tpl.slice(ePos + 2)
+        var op = f.charAt(0)
+        if (part1.length) con += sn + " += '" + tLine(part1) + "';"
+        switch (op){
+            case '=' :
+                f = f.slice(1)
+                con += sn + " += " + f + ";"
+                break
+            case '#' :
+                f = f.slice(1).trim()
+                con += sn +  " += etic2('" + f + "')(this);"
+                break
+            default:
+                con += f
+        }
+
+    }
+
+    tpl.length  && (con += sn + " += '" + tLine(tpl) + "';")
+    con += 'return ' + sn
+	return con
+
+}
+
 exports.insertTpl4JS = function(tpl_root , tpl_name ,  jsid, trans_mark ){
 	var tpl_path = path.resolve(tpl_root,tpl_name)
 	var tpl_body
-	if (false !== trans_mark) trans_mark = true
+	if (undefined === trans_mark) trans_mark = 1 
 	try{
 		tpl_body = fs.readFileSync(tpl_path).toString() 
 	}catch(err){
@@ -32,8 +70,13 @@ exports.insertTpl4JS = function(tpl_root , tpl_name ,  jsid, trans_mark ){
 	if (trans_mark && tpl_body){
 		tpl_body = tpl_body.replace(/<\%/g,'<?').replace(/\%>/g,'?>')
 	}
+	if (2 == trans_mark){
+		tpl_body = etic(tpl_body)
+		return `<script type="text/template-x" id="${jsid}">${tpl_body}</script>`
+		
+	}	
 
-	return `<script type="text/templage" id="${jsid}">${tpl_body}</script>`
+	return `<script type="text/template" id="${jsid}">${tpl_body}</script>`
 }
 exports.htmlEncode = function(str){
 	if (typeof str != 'string'){
